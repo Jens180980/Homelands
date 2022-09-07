3fb6ff1e5ffeff12ad2f372dfef18fc8ace08eff
Jens Harder <76961272+Jens180980@users.noreply.github.com>
2022-09-07 12:07:09 +0200

    employee created and init style

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import vejle from '../../Assets/img/vejle.png'
 import { Comments } from '../Partials/Comments'
-import { Employees } from '../Partials/Employees'
 import Style from '../../Assets/scss/Home.module.scss'
 import { CommentWrapper } from '../StateManagement/CommentData'
+import { EmployeeWrapper } from '../StateManagement/EmployeeData'
+import { EmployeeCard } from '../Partials/EmployeeCard'
 
 export const Home = () => {
   return (
-    <CommentWrapper>
       <section className={Style.home}> 
         <img src={vejle} alt="hero" className={Style.hero}></img>
         <div>
           <h2>Her kommer en slider</h2>
         </div>
-        <Comments />
-        <Employees />
+        <CommentWrapper>
+          <Comments />
+        </CommentWrapper>
+        <EmployeeWrapper>
+          <EmployeeCard />
+        </EmployeeWrapper> 
       </section>
-    </CommentWrapper>
+
   )
 }
